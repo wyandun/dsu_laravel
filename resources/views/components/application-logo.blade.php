833498cdc9dec1c068088f5f9025ba8a76dbd48c
wyandun <wilfyan@hotmail.com>
2025-07-18 14:18:37 -0500

    Update application-logo.blade.php

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,1 +1,1 @@
-<img src="{{ asset('logo.png') }}" alt="Logo" class="h-auto max-h-full w-auto max-w-full object-contain" {{ $attributes }}>
+<img src="{{ asset('logo.png') }}" alt="Logo" {{ $attributes }}>
